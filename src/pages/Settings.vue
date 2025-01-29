881f559f02ca7f8d4dba0c9251717637189c34b7
Fenish <sohretalhadev@outlook.com>
2025-01-29 12:09:06 +0300

    Add settings tab

---
+++ src/pages/Settings.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="wrapper">
+		<span>Settings Page</span>
+		<span>Config Dir: {{ configRef }}</span>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { appConfigDir } from '@tauri-apps/api/path'
+import { ref, onMounted } from 'vue'
+
+const configRef = ref<any>(null)
+
+onMounted(async () => {
+	configRef.value = await appConfigDir()
+})
+</script>
+
+<style scoped>
+@reference "tailwindcss";
+
+.wrapper {
+	@apply px-10;
+}
+</style>
